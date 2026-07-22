e0f245d63cbeb3219738fcf92227e85bb0f7fce0
satyapal49 <satyapalraj49@gmail.com>
2026-07-22 14:28:22 +0530

    updated

--- Function & Method/isPrime.java
+++ Function & Method/isPrime.java
@@ -3,13 +3,17 @@ import java.util.Scanner;
 public class isPrime {
     public static boolean isPrime(int num) {
         boolean isPrime = true;
-         for(int i = 2; i<= num-1; i++){
-            if(num % i == 0){
-                isPrime = false;
-                break;
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for(int i = 2; i<= Math.sqrt(num); i++){
+                if(num % i == 0){
+                    isPrime = false;
+                    break;
+                }
             }
-         }
-         return isPrime;
+        }
+        return isPrime;
     }
 
     public static void main(String[] args) {
