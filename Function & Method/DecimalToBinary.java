aee2bd4724de9e870bceb4454f5570121374435a
satyapal49 <satyapalraj49@gmail.com>
2026-08-12 11:49:46 +0530

    This program converts a decimal number to its binary representation

--- Function & Method/DecimalToBinary.java
+++ Function & Method/DecimalToBinary.java
@@ -5,14 +5,14 @@ public class DecimalToBinary {
     // This program converts a decimal number to its binary representation
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a decimal number: ");
-        int decimal = sc.nextInt();
-        int num = decimal;
-        String binary = "";
-        while (decimal > 0) {
-            int remainder = decimal % 2;
-            binary = remainder + binary;
-            decimal = decimal / 2;
+        System.out.println("Enter a decimal number: ");                 //  Prompt the user to enter a decimal number
+        int decimal = sc.nextInt();                                     // Read the decimal number input from the user
+        int num = decimal;                                               // Store the original decimal number for display purposes
+        String binary = "";                                             // Initialize an empty string to store the binary representation
+        while (decimal > 0) {                                             // Continue the loop until the decimal number becomes 0
+            int remainder = decimal % 2;                                // Calculate the remainder when the decimal number is divided by 2
+            binary = remainder + binary;                                   // Prepend the remainder to the binary string
+            decimal = decimal / 2;                                          // Update the decimal number by dividing it by 2
         }
         System.out.println("Binary representation of " + num + " is: " + binary);
     }
