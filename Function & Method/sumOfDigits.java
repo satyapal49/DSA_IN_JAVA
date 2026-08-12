167e0ffb3d452130daf1661dce68f71f4c0f1f3b
satyapal49 <satyapalraj49@gmail.com>
2026-08-12 11:07:19 +0530

    sum of digits of a given number

--- Function & Method/sumOfDigits.java
+++ Function & Method/sumOfDigits.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class sumOfDigits {
+
+    // This program calculates the sum of digits of a given number.
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int sum = 0;
+        while(num > 0){
+            int digit = num % 10;
+            sum = sum + digit;
+            num = num /10;
+        }
+        System.out.println(sum);
     }
 }
