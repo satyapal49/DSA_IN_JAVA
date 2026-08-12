8f09b84da80848336630c966d7b359150245b980
satyapal49 <satyapalraj49@gmail.com>
2026-08-12 14:26:57 +0530

    Hollow Rectangle Pattern

---
+++ Pattern II/HollowRectangle.java
@@ -0,0 +1,21 @@
+package pattern II;
+
+// Hollow Rectangle Pattern
+public class HollowRectangle {
+    public static void hollow_rectangle(int trows, int tcols) {
+        for (int i= 1; i <= trows; i++) {
+            for(int j = 1; j <= tcols; j++) {
+                if (i == 1 || i == trows || j == 1 || j == tcols) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        hollow_rectangle(4,5);
+    }
+}
