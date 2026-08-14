c0d59ee92eddc0dc98921037e0dc899796fee756
satyapal49 <satyapalraj49@gmail.com>
2026-08-14 11:16:52 +0530

    Hollow Rhombus Pattern

---
+++ PatternAdv/HollowRhombusPattern.java
@@ -0,0 +1,25 @@
+package PatternAdv;
+
+public class HollowRhombusPattern {
+    // Hollow Rhombus Pattern
+    public static void hollowRhombus(int n) {
+        for(int i =1 ; i<=n; i++){
+            // empty space
+            for(int j=1; j<= (n-i); j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j<= n; j++){
+                if(i == 1 || i == n || j==1 || j==n){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        hollowRhombus(5);
+    }
+}
